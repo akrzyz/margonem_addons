d7d09c0a28c735c77b1914847c7218e3b564adcb
akrzyz <aleksander.krzyz@gmail.com>
2015-12-06 12:11:55 +0100

    ovals opdate

--- oval.js
+++ oval.js
@@ -1,50 +1,62 @@
-var Oval = function(color)
-{
-  return $('<svg height="26" width="38" id="_svg"><ellipse id="_elipse" cx="19" cy="13" rx="18" ry="12" style="fill-opacity: 0.2; stroke: '+color+'; stroke-opacity: 0.5;" /></svg>')    
-    .css({position:"absolute", pointerEvents: 'none'})
-}
-
-var appendOvalBlure = function($oval, $source)
-{
-    $source
-    .mouseenter(function(){$oval.children().css({fillOpacity : 0.5, strokeOpacity : 1.0})})
-    .mouseleave(function(){$oval.children().css({fillOpacity : 0.2, strokeOpacity : 0.5})})
-}
-
 var wrapp = function(f1,f2)
 {
-	return function()
+    return function()
     {
-      	f1.apply(this,arguments)
+        f1.apply(this,arguments)
       	f2.apply(this,arguments)
     }
 }
 
-appendOvalNpc = function(npcs)
+//--- Oval ---
+var Oval = {}
+Oval.create = function(color)
+{
+    return $('<svg height="26" width="38"><ellipse id="_elipse" cx="19" cy="13" rx="18" ry="12" style="fill-opacity: 0.2; stroke: '+color+'; stroke-opacity: 0.5;" /></svg>')    
+    .css({position:"absolute", pointerEvents: 'none'})
+    .appendTo("#base")
+}
+Oval.blure = function($source)
+{
+    $source
+    .mouseenter(function(){this.children().css({fillOpacity : 0.5, strokeOpacity : 1.0})}.bind(this))
+    .mouseleave(function(){this.children().css({fillOpacity : 0.2, strokeOpacity : 0.5})}.bind(this))
+}
+Oval.update = function($source)
+{
+	this.css({left: parseInt($source.css("left")) - 3, top: parseInt($source.css("top")) + 32})
+}
+
+//--- oval ---
+var oval = {}
+//--- npc ---
+oval.npc = {}
+oval.npc.color = function(npc)
+{
+    if(npc.type == 2 || npc.type == 3)
+      return 'red'
+    return 'deepskyblue'
+}
+oval.npc.create = function(npc)
+{
+    return Oval.create(this.color(npc))
+}
+oval.npc.append = function(npcs)
 {
 	for(var i in npcs)
     {
       	if(npcs[i].lvl)
         {
             var $npc = $("#npc"+i)
-            var $oval = Oval(ovalNcpColor(npcs[i])).appendTo("#base")
+            var $oval = this.create(npcs[i])
 			npcs[i].$oval = $oval
-            appendOvalBlure($oval, $npc)
-    		updateOval($oval, $npc)
+            Oval.blure.call($oval, $npc)
+    		Oval.update.call($oval, $npc)
         }
     }
 }
-
-ovalNcpColor = function(npc)
-{
-	if(typeof npc.type === 'undefined')
-      return getOtherColor(npc)
-    if(npc.type == 2 || npc.type == 3)
-      return 'red'
-    return 'deepskyblue'
-}
-
-getOtherColor = function(other)
+//--- other ---
+oval.other = {}
+oval.other.color = function(other)
 {
   if(['en','cl-en','fr-en'].indexOf(other.relation) > -1)
     return 'red'
@@ -54,37 +66,25 @@ getOtherColor = function(other)
     return 'limegreen'
   return 'lightgray'
 }
-
-var updateOval = function($oval, $source)
+oval.other.create = function(other)
 {
-	$oval.css({left: parseInt($source.css("left")) - 3, top: parseInt($source.css("top")) + 32})
+    return Oval.create(this.color(other))
 }
-
-var config = {attributes: true, attributeFilter:['style']}
-var createObserver = function($source,$dest)
-{
-    
-	var observer = new MutationObserver(updateOval.bind(this,$dest,$source))
-    observer.observe($source.get(0),config)
-    return observer
-}
-
-var appendOvalOther = function(others)
+oval.other.append = function(others)
 {
     for(var i in others)
     {
         if(g.other[i] && !g.other[i].$oval)
         {
             var $other = $("#other"+i)
-            var $oval = Oval(ovalNcpColor(others[i])).appendTo("#base")
+            var $oval = this.create(others[i])
             g.other[i].$oval = $oval
-            appendOvalBlure($oval, $other)
-            createObserver($other,$oval)
+            Oval.blure.call($oval, $other)
+            ObserverFactory.create($other,$oval)
         }
     }
 }
-
-var removeOvalOther=function(others)
+oval.other.remove = function(others)
 {
     for(var i in others)
     {
@@ -92,19 +92,50 @@ var removeOvalOther=function(others)
              g.other[i].$oval.remove()
     }
 }
-
-var appendOvalHero = function()
+//--- hero ---
+oval.hero = {}
+oval.hero.color = function(){return "lime"}
+oval.hero.create = function(hero)
+{
+    return Oval.create(this.color(hero)) 
+}
+oval.hero.append = function()
 {
     var $hero = $("#hero")
-    var $oval = Oval('lime').appendTo("#base")
-    appendOvalBlure($oval, $hero)
-    createObserver($hero,$oval)
+    var $oval = this.create(hero)
+    Oval.blure.call($oval, $hero)
+    ObserverFactory.create($hero,$oval)
 }
 
-appendOvalHero()
-newNpc = wrapp(newNpc, appendOvalNpc)
-newOther = wrapp(removeOvalOther, newOther)
-newOther = wrapp(newOther, appendOvalOther)
+//--- observers ---
+var ObserverFactory = {}
+ObserverFactory.config = {attributes: true, attributeFilter:['style']}
+ObserverFactory.create = function($source,$dest)
+{
+	return this.create2($source, Oval.update.bind($dest, $source))
+}
+ObserverFactory.create2 = function($subject, update)
+{
+	var observer = new MutationObserver(update)
+    observer.observe($subject.get(0),this.config)
+    return observer
+}
 
-g.loadQueue.push({fun:function(){ message("oval")},data:""})
+//--- start ---
+oval.start = function()
+{
+    oval.hero.append()
+//    newNpc = wrapp(newNpc, function(o){oval.npc.append(o)})
+    newNpc = wrapp(newNpc, oval.npc.append.bind(oval.npc))
+    newOther = wrapp(oval.other.remove, newOther)
+//    newOther = wrapp(newOther, function(o){oval.other.append(o)})
+    newOther = wrapp(newOther, oval.other.append.bind(oval.other))
+}
+oval.start()
 
+//--- config ---
+oval.config = {}
+oval.config.display = function()
+{
+    mAllert("config",2)
+}
